84ca4acea565d8d717766019e3e7de0a0e457d8a
Laura Jones <ljones333@berkeley.edu>
2025-06-29 16:30:03 -0700

    added 'dead' button and styling

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ def index():
 def add():
 	name = request.form['name']
 	initiative = int(request.form['initiative'])
-	initiative_list.append({'name': name, 'initiative': initiative})
+	status = 'alive'
+	initiative_list.append({'name': name, 'initiative': initiative, 'status': status})
 	initiative_list.sort(key=lambda x: x['initiative'], reverse=True)
 	return redirect(url_for('index'))
 
@@ -30,6 +31,26 @@ def reset():
 	global initiative_list, current_turn
 	initiative_list = []
 	current_turn = 0
+	return redirect(url_for('index'))
+'''
+@app.route('/delete/<name>')
+def delete(name):
+#	completely removes a player from the list cause they died
+#		input: name of the player to die
+# 	return: index
+#
+	global initiative_list
+	new_list = [d for d in initiative_list if d.get('name') != name]
+	initiative_list = new_list
+	return redirect(url_for('index'))
+'''
+
+@app.route('/dead/<name>')
+def dead(name):
+	global initiative_list
+	for combatant in initiative_list:
+		if combatant['name'] == name:
+			combatant['status'] = 'dead'
 	return redirect(url_for('index'))
 
 if __name__ == '__main__':
