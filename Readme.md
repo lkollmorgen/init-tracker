17fb19c39ada4ca853640eea7517c2d9456a0797
Laura Jones <ljones333@berkeley.edu>
2025-07-03 00:39:43 -0700

    added player view, better UI

--- Readme.md
+++
@@ -1,5 +0,0 @@
-TODO
-- automatically update page at every global change
-- make 'tabletop view' so we can place a tablet flat on the table for both sides to see
-- make 'vertical view' so we can place a tablet upright and everyone can see in large letters/nums
-
--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
+DATA_FILE = 'initiative_data.json'
 
 app = Flask(__name__)
 
@@ -7,51 +9,81 @@ initiative_list = []
 current_turn = 0
 
 @app.route('/')
-def index():
-	return render_template('index.html', initiative_list = initiative_list, current_turn = current_turn)
+def home():
+    return redirect(url_for('player_view'))
+
+###### json data helper functions
+def save_data():
+	with open(DATA_FILE, 'w') as f:
+			json.dump({'initiative_list': initiative_list, 'current_turn':current_turn},f)
+	
+def load_data():
+	global initiative_list, current_turn
+	try:
+		with open(DATA_FILE, 'r') as f:
+				data = json.load(f)
+				initiative_list = data['initiative_list']
+				current_turn = data['current_turn']
+	except FileNotFoundError:
+		initiative_list = []
+		current_turn = 0
+
+@app.route('/player')
+def player_view():
+	load_data()
+	return render_template('player.html', initiative_list = initiative_list, current_turn = current_turn)
+
+@app.route('/admin')
+def admin_view():
+	load_data()
+	return render_template('admin.html', initiative_list=initiative_list, current_turn=current_turn)
 
 @app.route('/add', methods=['POST'])
 def add():
+	load_data()
 	name = request.form['name']
 	initiative = int(request.form['initiative'])
-	status = 'alive'
-	initiative_list.append({'name': name, 'initiative': initiative, 'status': status})
+	initiative_list.append({'name': name, 'initiative': initiative, 'status': 'alive'})
 	initiative_list.sort(key=lambda x: x['initiative'], reverse=True)
-	return redirect(url_for('index'))
+	save_data()
+	return redirect(url_for('admin_view'))
 
 @app.route('/next')
 def next_turn():
+	load_data()
 	global current_turn
 	if initiative_list:
 		current_turn = (current_turn + 1) % len(initiative_list)
-	return redirect (url_for('index'))
+		save_data()
+	return redirect (url_for('admin_view'))
+
+@app.route('/dead/<name>')
+def dead(name):
+	load_data()
+	global initiative_list
+	for combatant in initiative_list:
+		if combatant['name'] == name:
+			combatant['status'] = 'dead'
+	save_data()
+	return redirect(url_for('admin_view'))
+
+
+@app.route('/delete/<name>')
+def delete(name):
+	load_data()
+	global initiative_list
+	new_list = [d for d in initiative_list if d.get('name') != name]
+	initiative_list = new_list
+	save_data()
+	return redirect(url_for('admin_view'))
+
 
 @app.route('/reset')
 def reset():
 	global initiative_list, current_turn
 	initiative_list = []
 	current_turn = 0
-	return redirect(url_for('index'))
-'''
-@app.route('/delete/<name>')
-def delete(name):
-#	completely removes a player from the list cause they died
-#		input: name of the player to die
-# 	return: index
-#
-	global initiative_list
-	new_list = [d for d in initiative_list if d.get('name') != name]
-	initiative_list = new_list
-	return redirect(url_for('index'))
-'''
-
-@app.route('/dead/<name>')
-def dead(name):
-	global initiative_list
-	for combatant in initiative_list:
-		if combatant['name'] == name:
-			combatant['status'] = 'dead'
-	return redirect(url_for('index'))
+	return redirect(url_for('admin_view'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
